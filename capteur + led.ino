d6b7c84fac631d69cb5fdee24f003205a72a0863
ratoujemie8617 <51958809+ratoujemie8617@users.noreply.github.com>
2020-09-29 17:18:37 +0000

    Update and rename capteur.ino to capteur + led.ino

--- capteur + led.ino
+++ capteur + led.ino
@@ -1,8 +1,11 @@
 int Capteur_CO = A0;
+int LED = 5;
 
 void setup () {
   pinMode(Capteur_CO, INPUT);
 }
 void loop () {
-  
+  if(analogRead(Capteur_CO) <=500) {
+  digitalWrite (LED, HIGH);
+  }
 }
