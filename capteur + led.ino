5d977086fd0311cb71ce9609088bcfe19bed5873
ratoujemie8617 <51958809+ratoujemie8617@users.noreply.github.com>
2020-09-29 17:22:42 +0000

    Update capteur + led.ino

--- capteur + led.ino
+++ capteur + led.ino
@@ -3,9 +3,14 @@ int LED = 5;
 
 void setup () {
   pinMode(Capteur_CO, INPUT);
+  pinMode(LED, OUTPUT);
 }
 void loop () {
-  if(analogRead(Capteur_CO) <=500) {
+  if(analogRead(Capteur_CO) <500) {
+  digitalWrite (LED, LOW);
+  }
+  delay(1000);
+  if(analogRead(Capteur_CO) >500) {
   digitalWrite (LED, HIGH);
   }
 }
