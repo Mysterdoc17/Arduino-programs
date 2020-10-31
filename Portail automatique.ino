b3f56af5f7c5b88423be3a2c0d49d6d1d170dbeb
ratoujemie8617 <51958809+ratoujemie8617@users.noreply.github.com>
2020-10-31 17:44:57 +0100

    Update Portail automatique.ino

--- Portail automatique.ino
+++ Portail automatique.ino
@@ -1,7 +1,23 @@
-int BP_INT = 3;
-int capteur_inf = 4;
+int BP_INT = 3; // Bouton poussoir intérieur
+int capteur_inf = 4; // capteur infrarouge
+int moteur1 = 5; 
+int FDC_OUV = 6; // capteur pour indiquer que le portail est totalement ouvert
+int FDC_FER = 7; // capteur pour indiquer que le portail est totalement fermé
+int moteur2 = 2;
 
 void setup () {
+  Serial.begin(9600);
   pinMode(BP_INT, INPUT);
   pinMode(capteur_inf, INPUT);
+  pinMode(moteur1, OUTPUT);
+  pinMode(moteur2, OUTPUT);
+  pinMode(FDC_OUV, INPUT);
+  pinMode(FDC_FER, INPUT);
+}
+
+void loop () {
+  while (FDC_OUV != HIGH) {
+    digitalWrite(moteur1, HIGH);
+    digitalWrite(moteur2, LOW);
+  }
 }
