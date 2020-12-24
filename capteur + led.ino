a5c5639b671dae8dcdf2bec0de550105b4ed249e
Jeremie B <51958809+ratoujemie8617@users.noreply.github.com>
2020-12-24 19:21:59 +0100

    Update capteur + led.ino

--- capteur + led.ino
+++ capteur + led.ino
@@ -4,19 +4,19 @@ int LED = 5;
 void setup () {
   pinMode(Capteur_CO, INPUT);
   pinMode(LED, OUTPUT);
-  serial.begin(9600);
+  Serial.begin(9600);
 }
 void loop () {
   if(analogRead(Capteur_CO) <500) {
     digitalWrite (LED, LOW);
-    serial.print("ok");
+    Serial.print("ok");
   }
   delay(1000);
   if(analogRead(Capteur_CO) >500 <1000) {
     digitalWrite (LED, HIGH);
   }
   if(analogRead(Capteur_CO) >=1000) {
-  serial.print("Sortir de la maison/appartement immédiatement");
+  Serial.print("Sortir de la maison/appartement immédiatement");
     digitalWrite(LED,HIGH);
     delay(100);
     digitalWrite(LED,LOW);
